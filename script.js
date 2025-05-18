fc046f066a4418e5c0f6ea96c06edf8317e577c6
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-18 17:38:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  let lows1=s1.toLowerCase();
+  let lows2=s2.toLowerCase();
+
+	return lows1.indexOf(lows2);
 }
 
 // Please do not change the code below
